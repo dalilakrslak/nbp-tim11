f19cf5f4123f4aa98e044cdcb50087f1fab5af7a
ihasicic1 <ihasicic1@etf.unsa.ba>
2026-03-24 19:29:55 +0100

    add ticket based entities

---
+++ server/src/main/java/com/nbp/cinemaapp/entity/Hall.java
@@ -0,0 +1,100 @@
+package com.nbp.cinemaapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "halls")
+public class Hall {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "venue_id", nullable = false)
+    private Venue venue;
+
+    @OneToMany(
+            mappedBy = "hall",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    @JsonIgnore
+    private Set<Screening> screenings;
+
+    @Column
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @Column
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    @OneToMany(mappedBy = "hall", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private Set<Seat> seats;
+
+    public Hall() {}
+
+    public Hall(final UUID id,
+                final String name,
+                final Venue venue,
+                final Set<Screening> screenings,
+                final Instant createdAt,
+                final Instant updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.venue = venue;
+        this.screenings = screenings;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+
+    public Set<Screening> getScreenings() {
+        return screenings;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Set<Seat> getSeats() {
+        return seats;
+    }
+}
