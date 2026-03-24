c2812402264cc65245431978fba19396231aae11
ihasicic1 <ihasicic1@etf.unsa.ba>
2026-03-24 19:30:17 +0100

    add user and venue entities

---
+++ server/src/main/java/com/nbp/cinemaapp/entity/Venue.java
@@ -0,0 +1,127 @@
+package com.nbp.cinemaapp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "venues")
+public class Venue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(unique = true, nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String imageUrl;
+
+    @Column
+    @CreationTimestamp
+    private Instant createdAt;
+
+    @Column
+    @UpdateTimestamp
+    private Instant updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    @OneToMany(
+            mappedBy = "venue",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    @JsonIgnore
+    private List<Hall> halls;
+
+
+    public Venue() {}
+
+    public Venue(final UUID id,
+                 final String name,
+                 final String street,
+                 final String imageUrl,
+                 final Instant createdAt,
+                 final Instant updatedAt,
+                 final Location location,
+                 final List<Hall> halls) {
+        this.id = id;
+        this.name = name;
+        this.street = street;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.location = location;
+        this.halls = halls;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public List<Hall> getHalls() {
+        return halls;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+}
