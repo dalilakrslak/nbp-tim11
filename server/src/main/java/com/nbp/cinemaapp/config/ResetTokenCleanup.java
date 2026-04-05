7ce7b1494e0d7c909dbe6ca39415a57fcbf2ec43
ihasicic1 <ihasicic1@etf.unsa.ba>
2026-04-05 13:57:35 +0200

    update properties and add support for mail

---
+++ server/src/main/java/com/nbp/cinemaapp/config/ResetTokenCleanup.java
@@ -0,0 +1,26 @@
+package com.nbp.cinemaapp.config;
+
+import com.nbp.cinemaapp.repository.ResetTokenRepository;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@Configuration
+@EnableScheduling
+public class ResetTokenCleanup {
+
+    private final ResetTokenRepository resetTokenRepository;
+
+    public ResetTokenCleanup(final ResetTokenRepository resetTokenRepository) {
+        this.resetTokenRepository = resetTokenRepository;
+    }
+
+    @Scheduled(fixedRate = 3_600_000)
+    @Transactional
+    public void clearExpiredTokens() {
+        resetTokenRepository.deleteAllByExpiresAtBefore(LocalDateTime.now());
+    }
+}
