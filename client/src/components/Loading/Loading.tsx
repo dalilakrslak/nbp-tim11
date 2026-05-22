89883db331f5bf1e8cf41dc0b379d27b7f76a4ee
Nejra <nadilovic2@etf.unsa.ba>
2026-05-23 00:43:23 +0200

    See also list support

--- client/src/components/Loading/Loading.tsx
+++ client/src/components/Loading/Loading.tsx
@@ -1,23 +1,11 @@
-import { LoadingOutlined } from "@ant-design/icons";
-import { Skeleton, Spin } from "antd";
-import type { SpinProps } from "antd";
-
+import { Skeleton } from "antd";
 import "./loading.scss";
 
 export type LoadingProps = {
   rows?: number;
-  size?: SpinProps["size"];
 };
 
-export const Loading = ({ rows = 3, size }: LoadingProps) => {
-  if (size) {
-    return (
-      <div className="loading-container">
-        <Spin indicator={<LoadingOutlined spin />} size={size} />
-      </div>
-    );
-  }
-
+export const Loading = ({ rows = 3 }: LoadingProps) => {
   return (
     <div className="loading-container">
       <Skeleton active title={{ width: "60%" }} paragraph={{ rows }} />
