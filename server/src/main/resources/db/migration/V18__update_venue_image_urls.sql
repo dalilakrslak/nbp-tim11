30c83e2e7570f8dc08b0c3659849f32637011466
Dalila Kršlak <dkrslak1@etf.unsa.ba>
2026-05-22 21:20:43 +0200

    update url for venue images

---
+++ server/src/main/resources/db/migration/V18__update_venue_image_urls.sql
@@ -0,0 +1,49 @@
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/Cineplexx1.jpg'
+WHERE name = 'Cineplex'
+  AND street = 'Zmaja od Bosne 4, 71000 Sarajevo';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/cinestar-sarajevo.jpg'
+WHERE name = 'Cinestar'
+  AND street = 'Dzemala Bijedica St 160n, Sarajevo 71000';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/meeting-point.jpg'
+WHERE name = 'Kino Meeting Point'
+  AND street = 'Hamdije Kreševljakovića 13, Sarajevo 71000';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/kino-novi-grad.jpg'
+WHERE name = 'Kino Novi Grad Sarajevo'
+  AND street = 'Bulevar Meše Selimovića 97, Sarajevo 71000';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/kinoteka-BiH.jpg'
+WHERE name = 'Kinoteka Bosne i Hercegovine'
+  AND street = 'Alipašina 19, Sarajevo 71000';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/cinestar-tuzla.jpg'
+WHERE name = 'CineStar Tuzla'
+  AND street = 'Trg Slobode 1, Tuzla';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/cinestar-mostar.jpg'
+WHERE name = 'Kino Mostar'
+  AND street = 'Kneza Domagoja bb, Mostar';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/cineplexx-bl.jpg'
+WHERE name = 'Palas Cinema'
+  AND street = 'Kralja Petra I Karađorđevića 103, Banja Luka';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/zenica.jpg'
+WHERE name = 'Multiplex Ekran Zenica'
+  AND street = 'Kamberovića Polje bb, Zenica';
+
+UPDATE venues
+SET image_url = 'https://uzdlatfpubfuksghkzun.supabase.co/storage/v1/object/public/NBP/Venues/cineplexx-zagreb.jpg'
+WHERE name = 'Cineplexx Zagreb'
+  AND street = 'Slavonska Avenija 11D, Zagreb';
